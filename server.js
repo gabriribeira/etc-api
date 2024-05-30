a62974560f143f2b7c00f6153b3b29ea6b1b07be
gabriribeira <gmrribeira@gmail.com>
2024-05-30 10:25:34 +0100

    Updated Server file to handle all updates

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const cors = require("cors");
-// import OpenAI from "openai";
 const bodyParser = require("body-parser");
 const Sequelize = require("sequelize");
 require("dotenv").config();
 const routeManager = require("./app/routes/route.manager.js");
 const swaggerSpec = require("./app/config/swagger");
 const swaggerUi = require("swagger-ui-express");
-const passport = require('passport');
-const { jwtStrategy } = require('./app/config/passport');
+const passport = require("passport");
+const http = require("http");
+require("./app/config/mongo");
+const socketIo = require("socket.io");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 var corsOptions = {
-  origin: "http://localhost:3001",
+  origin: process.env.PLATFORM_FRONTEND_URL,
+  credentials: true,
 };
 
 const app = express();
+const server = http.createServer(app);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -46,21 +51,79 @@ db.sequelize
     console.log("Failed to sync db: " + err.message);
   });
 
+const io = socketIo(server, {
+  cors: {
+    origin: process.env.PLATFORM_FRONTEND_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+const sessionMiddleware = session({
+  secret: process.env.JWT_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: "mongodb://localhost:27017/etc" }),
+});
+
 //middlewares
+app.use(sessionMiddleware);
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.disable("x-powered-by");
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+io.use((socket, next) => {
+  sessionMiddleware(socket.request, {}, next);
+});
+
+io.on("connection", async (socket) => {
+  console.log("A client connected");
+
+  const userId = socket.request.session.passport?.user;
+  if (!userId) {
+    socket.emit("authentication_error", "User is not authenticated");
+    return;
+  }
+
+  try {
+    const user = await db.User.findByPk(userId, { include: [db.Household] });
+
+    if (!user) throw new Error("User not found");
+
+    const userHouseholds = user.Households.map((household) => household.id);
+
+    userHouseholds.forEach((householdId) => {
+      socket.join(`household-${householdId}`);
+    });
+
+    console.log(
+      `User ${userId} authenticated with households ${userHouseholds}`
+    );
+  } catch (error) {
+    console.error("Authentication error:", error.message);
+    socket.emit("authentication_error", error.message);
+  }
+
+  socket.on("disconnect", () => {
+    console.log("A client disconnected");
+  });
+});
+
+function emitNotificationToHousehold(householdId, notification) {
+  io.to(`household-${householdId}`).emit("notification", notification);
+}
+
+module.exports = { io, emitNotificationToHousehold };
 
 routeManager(app);
 
 //port
 const PORT = process.env.PORT || 3001;
 
-//server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on ${process.env.PLATFORM_BACKEND_URL}`);
 });

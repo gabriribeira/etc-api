887dabda5bb70ab4ad61c6a89f4a87c1210b6483
gabriribeira <gmrribeira@gmail.com>
2024-06-17 20:18:15 +0100

    commented mongo

--- config/mongo.js
+++ config/mongo.js
@@ -1,15 +1,15 @@
 // app.js or index.js
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/etc', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect('mongodb://localhost:27017/etc', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
+// mongoose.connection.on('connected', () => {
+//   console.log('Connected to MongoDB');
+// });
 
-mongoose.connection.on('error', (err) => {
-  console.error('Error connecting to MongoDB:', err);
-});
+// mongoose.connection.on('error', (err) => {
+//   console.error('Error connecting to MongoDB:', err);
+// });

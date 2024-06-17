7ed1e33a79f23f887a65f5b1ef7c3aff4a8e62db
gabriribeira <gmrribeira@gmail.com>
2024-06-17 14:43:38 +0100

    Updated server index file with local vs online changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const http = require("http");
 require("./config/mongo.js");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const path = require('path');
+const path = require("path");
 
 var corsOptions = {
   origin: process.env.PLATFORM_FRONTEND_URL,
@@ -26,8 +26,8 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: "localhost",
-    dialect: "mysql",
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   }
 );
 
@@ -56,10 +56,11 @@ const sessionMiddleware = session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
-    sameSite: 'Lax',
-  }
+    secure: false,
+    //   httpOnly: true,
+    //   sameSite: 'Lax'
+  },
+  //process.env.NODE_ENV === 'production'
   // store: MongoStore.create({ mongoUrl: "mongodb://localhost:27017/etc" }),
 });
 
@@ -75,59 +76,15 @@ app.disable("x-powered-by");
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Serve static files from the React app located in public_html
-app.use(express.static(path.join(__dirname, '../public_html')));
+app.use(express.static(path.join(__dirname, "../public_html")));
 
 // Ensure the API routes are handled before the catch-all handler
 routeManager(app);
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public_html', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public_html", "index.html"));
 });
-
-// Uncomment and configure socket.io if necessary
-// io.use((socket, next) => {
-//   sessionMiddleware(socket.request, {}, next);
-// });
-
-// io.on("connection", async (socket) => {
-//   console.log("A client connected");
-
-//   const userId = socket.request.session.passport?.user;
-//   if (!userId) {
-//     socket.emit("authentication_error", "User is not authenticated");
-//     return;
-//   }
-
-//   try {
-//     const user = await db.User.findByPk(userId, { include: [db.Household] });
-
-//     if (!user) throw new Error("User not found");
-
-//     const userHouseholds = user.Households.map((household) => household.id);
-
-//     userHouseholds.forEach((householdId) => {
-//       socket.join(`household-${householdId}`);
-//     });
-
-//     console.log(
-//       `User ${userId} authenticated with households ${userHouseholds}`
-//     );
-//   } catch (error) {
-//     console.error("Authentication error:", error.message);
-//     socket.emit("authentication_error", error.message);
-//   }
-
-//   socket.on("disconnect", () => {
-//     console.log("A client disconnected");
-//   });
-// });
-
-// function emitNotificationToHousehold(householdId, notification) {
-//   io.to(`household-${householdId}`).emit("notification", notification);
-// }
-
-// module.exports = { io, emitNotificationToHousehold };
 
 // Port
 const PORT = process.env.PORT || 3001;

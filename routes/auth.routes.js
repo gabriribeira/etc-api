a4d3f3b7a9a6f75a405d2399849c2fb1b672a710
gabriribeira <gmrribeira@gmail.com>
2024-06-17 14:37:51 +0100

    Updated Auth Routes for Local Strategy

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 async function createPrivateHousehold(user) {
   const household = await Household.create({
-    name: `${user.name}'s Household`,
+    name: `${user.name ? user.name : user.email.split("@")[0]}'s Household`,
     privacy: true,
   });
 
@@ -38,17 +38,31 @@ async function getOrCreatePrivateHousehold(user) {
 
 router.post(
   "/login",
-  passport.authenticate("local", { failureRedirect: process.env.SUCCESS_REDIRECT_URL}),
-  async function (req, res) {
-    const user = req.user;
-
-    if (user) {
-      const { household, roleId } = await getOrCreatePrivateHousehold(user);
-      req.session.currentHouseholdId = household.dataValues.id;
-      req.session.roleId = roleId;
-    }
-
-    res.status(200).json(user);
+  (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
+      if (err) {
+        console.error("Login error:", err);
+        return next(err);
+      }
+      if (!user) {
+        console.warn("Login failed:", info.message);
+        return res.status(401).json({ message: info.message });
+      }
+      req.logIn(user, async (err) => {
+        if (err) {
+          console.error("Login session error:", err);
+          return next(err);
+        }
+        const { household, roleId } = await getOrCreatePrivateHousehold(user);
+        req.session.currentHouseholdId = household.dataValues.id;
+        req.session.roleId = roleId;
+        res.status(200).json({
+          user,
+          currentHouseholdId: req.session.currentHouseholdId,
+          roleId: req.session.roleId,
+        });
+      });
+    })(req, res, next);
   }
 );
 
@@ -66,7 +80,7 @@ router.post("/register", (req, res, next) => {
 
       const household = await createPrivateHousehold(user);
 
-      req.login(user, (err) => {
+      req.logIn(user, (err) => {
         if (err) {
           return res
             .status(500)
@@ -74,7 +88,11 @@ router.post("/register", (req, res, next) => {
         }
         req.session.currentHouseholdId = household.id;
         req.session.roleId = 2;
-        return res.status(201).json(user);
+        return res.status(201).json({
+          user,
+          currentHouseholdId: req.session.currentHouseholdId,
+          roleId: req.session.roleId,
+        });
       });
     } catch (error) {
       res.status(400).json({ message: "Error creating user", error });
@@ -119,7 +137,6 @@ router.get(
 );
 
 router.get("/checkAuth", (req, res) => {
-  console.log(req);
   if (req.isAuthenticated()) {
     res.status(200).json({
       authenticated: true,

3e0a739a2eeec4244986f63d8c93e50c93e8312e
gabriribeira <gmrribeira@gmail.com>
2024-06-19 18:18:24 +0100

    Removed User Update validations to be for each field

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -8,7 +8,4 @@ exports.validateCreateUser = [
 // Validation middleware for updating an existing user
 exports.validateUpdateUser = [
   body("name").optional().isLength({ max: 64 }).withMessage("Name must be between 2 and 64 characters"),
-  body("username").optional().isLength({ min: 3, max: 28 }).withMessage("Username must be between 3 and 28 characters"),
-  body("email").optional().isEmail().withMessage("Invalid email format"),
-  body("description").optional().isLength({ max: 512 }).withMessage("Description cannot exceed 512 characters"),
 ];
